ad779ebba3560bc59717f8db68f5647ca1d48fa3
Gaofang <845469177@qq.com>
2025-11-12 15:43:05 +0800

    登录页面补全

--- project/src/views/Login/index.vue
+++ project/src/views/Login/index.vue
@@ -1,9 +1,91 @@
 <template>
-  <div>登录页</div>
+  <div class="app_login">
+    <el-card class="box_card" header="后台管理系统" shadow="hover">
+      <el-form
+        :model="loginForm"
+        status-icon
+        :rules="rules"
+        ref="loginForm"
+        label-width="100px"
+        class="loginForm"
+      >
+        <el-form-item label="用户名：" prop="username">
+          <el-input v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码：" prop="password">
+          <el-input type="password" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('loginForm')"
+            >提交</el-button
+          >
+          <el-button @click="resetForm('loginForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Login",
+  data() {
+    var validateUsername = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else {
+        callback();
+      }
+    };
+    var validatePassword = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [{ validator: validatePassword, trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("提交成功");
+        } else {
+          console.log("提交失败");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.app_login {
+  width: 100%;
+  height: 100%;
+  background-image: url("../../assets/image/bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.box_card {
+  width: 460px;
+  height: 260px;
+}
+</style>
